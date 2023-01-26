848dab68375209d373eb65b6b485e157c8cfb33c
Akadenza <61107208+seelebrn@users.noreply.github.com>
2023-01-26 14:17:41 +0100

    Create TaiwuETC.cs

---
+++ TaiwuETC.cs
@@ -0,0 +1,561 @@
+using BepInEx;
+using Config;
+using GameData.Domains.Character;
+using HarmonyLib;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+using TaiwuModdingLib;
+using TaiwuModdingLib.Core.Plugin;
+
+namespace TaiwuETC
+{
+    [PluginConfig("TaiwuETC", "Taiwu Mods Community", "0.1.0")]
+    public class Main : TaiwuRemakeHarmonyPlugin
+    {
+        public static Dictionary<string, string> translationDict;
+        public static Dictionary<string, string> NamesDict;
+        public static Dictionary<string, string> SurnamesDict;
+        public static List<string> MissingNames = new List<string>();
+        public static List<string> MissingSurnames = new List<string>();
+        public static string translationpath = Path.Combine("Mod", "TaiwuETC", "Plugins", "Translations");
+
+        public static Dictionary<string, string> FileToDictionary(string dir)
+        {
+
+
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+
+            IEnumerable<string> lines = File.ReadLines(Path.Combine("Mod", "TaiwuETC", "Plugins", "Translations", dir));
+
+            foreach (string line in lines)
+            {
+
+                var arr = line.Split('¤');
+                if (arr[0] != arr[1])
+                {
+                    var pair = new KeyValuePair<string, string>(Regex.Replace(arr[0], @"\t|\n|\r", ""), arr[1]);
+
+                    if (!dict.ContainsKey(pair.Key))
+                        dict.Add(pair.Key, pair.Value);
+                    else
+                        Debug.Log($"Found a duplicated line while parsing {dir}: {pair.Key}");
+
+
+
+                }
+            }
+
+            return dict;
+
+        }
+
+
+        public override void OnModSettingUpdate()
+        {
+            base.OnModSettingUpdate();
+        }
+
+        public override void Initialize()
+        {
+            try
+            {
+
+                SurnamesDict = FileToDictionary("Surnames.txt");
+                NamesDict = FileToDictionary("Names.txt");
+                translationDict = SurnamesDict.MergeLeft(NamesDict);
+                Debug.Log("Successfully Generated Dict");
+            }
+
+            catch (Exception e)
+            {
+                Debug.Log("Error in generating dicts");
+                Debug.LogException(e);
+            }
+            Debug.Log("!!!!! Taiwu ETC loaded");
+            this.OnModSettingUpdate();
+            Harmony harmony = new Harmony("TaiwuETC.Taiwu Mods Community.0.1.0");
+            harmony.PatchAll();
+        }
+
+        public void Awake()
+        {
+            //Preparing FailedRegistry.txt - Step 1
+
+           
+
+           
+
+        }
+        private void Update()
+        {
+            if(Input.GetKeyUp(KeyCode.F11))
+            {
+                File.WriteAllText(Path.Combine(translationpath, "MissingNames.txt"), String.Empty);
+                File.WriteAllText(Path.Combine(translationpath, "MissingSurnames.txt"), String.Empty);
+                    foreach (string s in MissingNames.Distinct())
+                    {
+                    using (StreamWriter sw = File.AppendText(Path.Combine(translationpath, "MissingNames.txt")))
+                        if (Helpers.IsChinese(s))
+                        {
+                            sw.Write(s);
+                        }
+                }
+                foreach(string s in MissingSurnames.Distinct())
+                {
+                    using (StreamWriter sw = File.AppendText(Path.Combine(translationpath, "MissingSurnames.txt")))
+                    {
+                        if(Helpers.IsChinese(s))
+                        { 
+                        sw.Write(s);
+                        }
+                    }
+                }
+                
+            }
+        }
+    }
+        
+    /*
+    [HarmonyPatch(typeof(MonasticTitleItem), "MonasticTitleItem")]
+    static class Patch01
+    {
+        static void Postfix(MonasticTitleItem __instance)
+        {
+            Debug.Log(__instance.Name);
+        }
+    }*/
+    [HarmonyPatch(typeof(LocalMonasticTitles), "Init")]
+
+    static class LocalMonasticTitles_Patch
+    {
+
+        static void Postfix(LocalMonasticTitles __instance)
+        {
+            foreach (MonasticTitleItem x in __instance.MonasticTitles)
+            {
+                try
+                {
+                    if (x.Name != null)
+                    {
+
+                        if (x.Name != "" && Main.translationDict.ContainsKey(x.Name))
+                        {
+                            // Debug.Log("Found one ! : " + x.Name);
+                            FieldInfo fi = x.GetType().GetField("Name");
+                            fi.SetValue(x, Main.translationDict[x.Name]);
+                            // Debug.Log("Modified Name = " + x.Name);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+        }
+    }
+    [HarmonyPatch(typeof(LocalTownNames), "Init")]
+
+    static class LocalTownNames_Patch
+    {
+
+        static void Postfix(LocalTownNames __instance)
+        {
+            foreach (TownNameItem x in __instance.TownNameCore)
+            {
+                try
+                {
+                    if (x.Name != null)
+                    {
+
+                        if (x.Name != "" && Main.translationDict.ContainsKey(x.Name))
+                        {
+                            //Debug.Log("Found one Surname ! : " + x.Name);
+                            FieldInfo fi = x.GetType().GetField("Name");
+                            fi.SetValue(x, Main.translationDict[x.Name]);
+                            //Debug.Log("Modified Name = " + x.Name);
+                        }
+
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+        }
+    }
+    [HarmonyPatch(typeof(LocalNames), "Init")]
+
+    static class LocalNames_Patch
+    {
+
+        static void Postfix(LocalNames __instance)
+        {
+            foreach (HanNameItem x in __instance.AllNamesCore)
+            {
+                try
+                { 
+                if(x.MiddleChar != null)
+                    {
+                        if(Main.translationDict.ContainsKey(x.MiddleChar))
+                        {
+                            x.GetType().GetField("MiddleChar").SetValue(x, Main.translationDict[x.MiddleChar]);
+                        }
+                        else
+                        {
+                            Main.MissingNames.Add(x.MiddleChar);
+                        }
+                    }
+                for (int i = 0; i < x.ApartMan.Count(); i++)
+                {
+                    if (x.ApartMan[i] != null)
+                    {
+                        var y = x.ApartMan[i];
+                        if (y != "" && Main.translationDict.ContainsKey(y))
+                        {
+
+                            FieldInfo fi = x.GetType().GetField("ApartMan");
+                            string[] value = (string[])fi.GetValue(x);
+                            for (int j = 0; j < value.Count(); j++)
+                            {
+                                if (Main.translationDict.ContainsKey(value[j]))
+                                {
+                                    //Debug.Log("Found one Name ! : " + value[j]);
+                                    value[j] = Main.translationDict[value[j]];
+                                    //Debug.Log("Modified Name = " + value[j]);
+                                }
+                            }
+                        }
+                        else
+                        {
+
+
+                            if (y != null && y != "")
+                            {
+                                Main.MissingNames.Add(y);
+                            }
+
+                        }
+                    }
+                }
+
+                for (int i = 0; i < x.ApartNeutral.Count(); i++)
+                {
+                    if (x.ApartNeutral[i] != null)
+                    {
+                        var y = x.ApartNeutral[i];
+                        if (y != "" && Main.translationDict.ContainsKey(y))
+                        {
+
+                            FieldInfo fi = x.GetType().GetField("ApartNeutral");
+                            string[] value = (string[])fi.GetValue(x);
+                            for (int j = 0; j < value.Count(); j++)
+                            {
+                                if (Main.translationDict.ContainsKey(value[j]))
+                                {
+                                    //Debug.Log("Found one Name ! : " + value[j]);
+                                    value[j] = Main.translationDict[value[j]];
+                                    //Debug.Log("Modified Name = " + value[j]);
+                                }
+                            }
+                        }
+                        else
+                        {
+
+
+                            if (y != null && y != "")
+                            {
+                                Main.MissingNames.Add(y);
+                            }
+
+                        }
+                    }
+                }
+
+                for (int i = 0; i < x.ApartWoman.Count(); i++)
+                {
+                    if (x.ApartWoman[i] != null)
+                    {
+                        var y = x.ApartWoman[i];
+                        if (y != "" && Main.translationDict.ContainsKey(y))
+                        {
+
+                            FieldInfo fi = x.GetType().GetField("ApartWoman");
+                            string[] value = (string[])fi.GetValue(x);
+                            for (int j = 0; j < value.Count(); j++)
+                            {
+                                if (Main.translationDict.ContainsKey(value[j]))
+                                {
+                                    //Debug.Log("Found one Name ! : " + value[j]);
+                                    value[j] = Main.translationDict[value[j]];
+                                    //Debug.Log("Modified Name = " + value[j]);
+                                }
+                            }
+                        }
+                        else
+                        {
+
+
+                            if (y != null && y != "")
+                            {
+                                Main.MissingNames.Add(y);
+                            }
+
+                        }
+                    }
+                }
+
+                for (int i = 0; i < x.SerialMan.Count(); i++)
+                {
+                    if (x.SerialMan[i] != null)
+                    {
+                        var y = x.SerialMan[i];
+                        if (y != "" && Main.translationDict.ContainsKey(y))
+                        {
+
+                            FieldInfo fi = x.GetType().GetField("SerialMan");
+                            string[] value = (string[])fi.GetValue(x);
+                            for (int j = 0; j < value.Count(); j++)
+                            {
+                                if (Main.translationDict.ContainsKey(value[j]))
+                                {
+                                    //Debug.Log("Found one Name ! : " + value[j]);
+                                    value[j] = Main.translationDict[value[j]];
+                                    //Debug.Log("Modified Name = " + value[j]);
+                                }
+                            }
+                        }
+                        else
+                        {
+
+
+                            if (y != null && y != "")
+                            {
+                                Main.MissingNames.Add(y);
+                            }
+
+                        }
+                    }
+                }
+
+                for (int i = 0; i < x.SerialNeutral.Count(); i++)
+                {
+                    if (x.SerialNeutral[i] != null)
+                    {
+                        var y = x.SerialNeutral[i];
+                        if (y != "" && Main.translationDict.ContainsKey(y))
+                        {
+
+                            FieldInfo fi = x.GetType().GetField("SerialNeutral");
+                            string[] value = (string[])fi.GetValue(x);
+                            for (int j = 0; j < value.Count(); j++)
+                            {
+                                if (Main.translationDict.ContainsKey(value[j]))
+                                {
+                                    //Debug.Log("Found one Name ! : " + value[j]);
+                                    value[j] = Main.translationDict[value[j]];
+                                   //Debug.Log("Modified Name = " + value[j]);
+                                }
+                            }
+                        }
+                        else
+                        {
+
+
+                            if (y != null && y != "")
+                            {
+                                Main.MissingNames.Add(y);
+                            }
+
+                        }
+                    }
+                }
+
+                for (int i = 0; i < x.SerialWoman.Count(); i++)
+                {
+                    if (x.SerialWoman[i] != null)
+                    {
+                        var y = x.SerialWoman[i];
+                        if (y != "" && Main.translationDict.ContainsKey(y))
+                        {
+
+                            FieldInfo fi = x.GetType().GetField("SerialWoman");
+                            string[] value = (string[])fi.GetValue(x);
+                            for (int j = 0; j < value.Count(); j++)
+                            {
+                                if (Main.translationDict.ContainsKey(value[j]))
+                                {
+                                    //Debug.Log("Found one Name ! : " + value[j]);
+                                    value[j] = Main.translationDict[value[j]];
+                                    //Debug.Log("Modified Name = " + value[j]);
+                                }
+                            }
+                        }
+                        else
+                        {
+
+
+                            if (y != null && y != "")
+                            {
+                                Main.MissingNames.Add(y);
+                            }
+
+                        }
+                    }
+                }
+
+                }
+                catch
+                { }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(LocalSurnames), "Init")]
+
+    static class LocalSurNames_Patch
+    {
+
+        static void Postfix(LocalSurnames __instance)
+        {
+            foreach (SurnameItem x in __instance.SurnameCore)
+            {
+                try
+                {
+
+                    if (x.Surname != null)
+                    {
+
+                        if (x.Surname != "" && Main.translationDict.ContainsKey(x.Surname))
+                        {
+                            //Debug.Log("Found one ! : " + x.Surname);
+                            FieldInfo fi = x.GetType().GetField("Surname");
+                            fi.SetValue(x, Main.translationDict[x.Surname]);
+                            //Debug.Log("Modified Name = " + x.Surname);
+                        }
+                        else
+                        {
+                            if (x.Surname != null && x.Surname != "")
+                            {
+                                Main.MissingSurnames.Add(x.Surname);
+                            }
+                        }
+                    }
+                }
+
+                catch (Exception e)
+                {
+
+                }
+
+
+
+            }
+        }
+    }
+
+
+    [HarmonyPatch(typeof(LocalStringManager), "Get", new Type[] { typeof(ushort)})]
+    static class FullName_Patch
+    {
+        static void Postfix(LocalStringManager __instance, ref ushort id, ref string __result)
+        {
+            if(__result == "Taiwu")
+            {
+                __result = "Taiwu ";
+            }
+        }
+    }
+    /*[HarmonyPatch(typeof(NameCenter), "GetMonasticTitleOrName")]
+    static class NameCenter_Patch_01
+    {
+        static void Postfix(NameCenter __instance, ref ValueTuple<string, string> __result)
+        {
+            __result.Item2 = " " + __result.Item2;
+        }
+    }*/
+
+    [HarmonyPatch(typeof(NameCenter), "GetName")]
+    static class NameCenter_Patch_02
+    {
+        static void Postfix(NameCenter __instance, ref ValueTuple<string, string> __result)
+        {
+            __result.Item2 = " " + __result.Item2;
+        }
+    }
+    [HarmonyPatch(typeof(NameCenter), "HasInvalidCharForName")]
+    static class REGEX_Patch
+    {
+        static void Postfix(ref string nameString, ref bool __result )
+        {
+
+            //Debug.Log("NameString = " + nameString);
+            //Debug.Log("Result = " + __result);
+            Regex InvalidCharRegex = new Regex("[\\u0022\\d~!@#$%^&*()_+-=\\[\\]{}\\\\|;:',.<>/?·`！￥…（）—、【】：；‘’“”《》，。？]");
+            __result = InvalidCharRegex.IsMatch(nameString);
+        }
+    }
+
+    [HarmonyPatch(typeof(FullName), "GetName")]
+    static class FullName_GetName_Patch
+    {
+        static void Postfix(FullName __instance, ref ValueTuple<string, string> __result)
+        {
+
+                __result.Item2 = __result.Item2.ToLower();
+                __result.Item2 = char.ToUpper(__result.Item2[0]) + __result.Item2.Substring(1);
+                //Debug.Log("Result.Item2 = " + __result.Item2);
+
+        }
+    }
+
+    public static class DictionaryExtensions
+    {
+
+        public static T MergeLeft<T, K, V>(this T me, params IDictionary<K, V>[] others)
+            where T : IDictionary<K, V>, new()
+        {
+            T newMap = new T();
+            foreach (IDictionary<K, V> src in
+                (new List<IDictionary<K, V>> { me }).Concat(others))
+            {
+                // ^-- echk. Not quite there type-system.
+                foreach (KeyValuePair<K, V> p in src)
+                {
+                    newMap[p.Key] = p.Value;
+                }
+            }
+            return newMap;
+        }
+
+        public static Dictionary<TKey, TValue>
+        Merge<TKey, TValue>(IEnumerable<Dictionary<TKey, TValue>> dictionaries)
+        {
+            var result = new Dictionary<TKey, TValue>(dictionaries.First().Comparer);
+            foreach (var dict in dictionaries)
+                foreach (var x in dict)
+                    result[x.Key] = x.Value;
+            return result;
+        }
+
+    }
+     public static class Helpers
+    {
+        public static readonly Regex cjkCharRegex = new Regex(@"\p{IsCJKUnifiedIdeographs}");
+        public static bool IsChinese(string s)
+        {
+            return cjkCharRegex.IsMatch(s);
+        }
+    }
+}
